7d891379f00b59ef72048a4d702c6ada85383d75
Muhammed Hedyia <hedyia@hotmail.com>
2018-09-18 08:36:09 +0200

    AddGoingScript

---
+++ GigHub/Controllers/Api/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddGigToCalendar(GigDto gigDto)
+        {
+            var userId = User.Identity.GetUserId();
+            var exists = _context.Attendances
+                .Any(a => a.GigId == gigDto.GigId && a.AttendeeId == userId);
+            if (exists)
+            {
+                return BadRequest("The Attendance already exists.");
+            }
+            var attendance = new Attendance
+            {
+                GigId = gigDto.GigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/Dtos/GigDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GigHub.Dtos
+{
+    public class GigDto
+    {
+        public int GigId { get; set; }
+    }
+}
